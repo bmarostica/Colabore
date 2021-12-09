b445d97af4b305d18bf6e0bb71ce5d8b7857d71b
bianca.marostica <bmarostica@gmail.com>
2021-12-09 17:18:26 -0300

    application

---
+++ colabore/src/main/java/com/dbc/colabore/entity/CampanhaEntity.java
@@ -0,0 +1,18 @@
+package com.dbc.colabore.entity;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class CampanhaEntity {
+
+    private String foto;
+    private String tituloCampanha;
+    private BigDecimal metaArrecadacao;
+    private BigDecimal totalArrecadado;
+    private List<String> tagsCategoria;
+    private LocalDateTime ultimaAlteracao;
+
+
+
+}
