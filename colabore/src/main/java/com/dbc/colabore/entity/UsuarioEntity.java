c016740ca341cbdc5cd602a0b1ef0161cf76b549
bianca.marostica <bmarostica@gmail.com>
2021-12-09 20:35:34 -0300

    adicionado no usuario entiry os metodos para autenticação

--- colabore/src/main/java/com/dbc/colabore/entity/UsuarioEntity.java
+++ colabore/src/main/java/com/dbc/colabore/entity/UsuarioEntity.java
@@ -1,22 +1,54 @@
 package com.dbc.colabore.entity;
 
-import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
+import java.util.Collection;
+
 
 @Getter
 @Setter
-@NoArgsConstructor
-@RequiredArgsConstructor
-public class UsuarioEntity {
+public class UsuarioEntity implements UserDetails {
     private Integer idUsuario;
     private String nome;
     private String email;
     private String senha;
     private String fotoPerfil;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null; //TODO necessário implementar as regras para o usuário
+    }
+
+    @Override
+    public String getPassword() {
+        return senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return nome;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
