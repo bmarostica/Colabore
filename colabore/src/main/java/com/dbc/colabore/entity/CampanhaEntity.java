ec68a081baba0b907bad330df3637979a07d7d2d
bianca.marostica <bmarostica@gmail.com>
2021-12-09 17:35:57 -0300

    editada classe CampanhaEntity

--- colabore/src/main/java/com/dbc/colabore/entity/CampanhaEntity.java
+++ colabore/src/main/java/com/dbc/colabore/entity/CampanhaEntity.java
@@ -1,11 +1,21 @@
 package com.dbc.colabore.entity;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@RequiredArgsConstructor
 public class CampanhaEntity {
 
+    private Integer idCampanha;
     private String foto;
     private String tituloCampanha;
     private BigDecimal metaArrecadacao;
